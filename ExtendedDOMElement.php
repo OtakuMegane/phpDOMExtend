914c2164c67806aba15cdbe71b09949155a20b9a
OtakuMegane <otakumegane@insaneotaku.com>
2017-12-20 15:08:36 -0500

    Improvements

--- ExtendedDOMElement.php
+++ ExtendedDOMElement.php
@@ -127,7 +127,7 @@ class ExtendedDOMElement extends \DOMElement
 
     public function getElementById($id)
     {
-        return $this->doXPathQuery(".//*[@id='" . $id . "']", $this)->item(0);
+        return $this->doXPathQuery("(.//*[@id='" . $id . "'])[1]", $this)->item(0);
     }
 
     public function getElementsByAttributeName($attribute_name)
